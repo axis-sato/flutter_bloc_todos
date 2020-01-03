73ba466b06cee8544e04a5b639c403ca9db47dd2
sato <sato_m@mediaxis.jp>
2020-01-03 20:48:22 +0900

    App Keys

---
+++ lib/flutter_todos_keys.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class FlutterTodosKeys {
+  static final extractActionsPopupMenuButton =
+      const Key('__extractActionsPopupMenuButton__');
+  static final extraActionsEmptyContainer =
+      const Key('__extraActionsEmptyContainer__');
+  static final filteredTodosEmptyContainer =
+      const Key('__filteredTodosEmptyContainer__');
+  static final statsLoadingIndicator = const Key('__statsLoadingIndicator__');
+  static final emptyStatsContainer = const Key('__emptyStatsContainer__');
+  static final emptyDetailsContainer = const Key('__emptyDetailsContainer__');
+  static final detailsScreenCheckBox = const Key('__detailsScreenCheckBox__');
+}
